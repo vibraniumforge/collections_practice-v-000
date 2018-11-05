cdee2db3cf982f65213281fe1c03ae3cc3e3e905
Greg Morgan <gregm9999@gmail.com>
2018-11-05 01:38:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,7 @@
 require "pry"
 
 def sort_array_asc(arr)
-  arr.sort do |a,b|
-    a<=>b
-  end
+  arr.sort
 end
 
 def sort_array_desc(arr)
@@ -18,38 +16,27 @@ def sort_array_char_count(arr)
   end
 end
 
-def swap_elements(arr)
-  arr.sort do |a,b|
-    a.length<=> b.length
-  end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
-# def swap_elements_from_to (arr, index, destination_index)
-#   arr.sort do |a,b|
-#     ar[index]<=>ar[destination_index]
-#   end
-#   puts arr
-# end
+def swap_elements_from_to (array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
 
 def reverse_array(arr)
-  newAr=[]
-  arr.map do |a,b|
-    newAr=arr.reverse
-  end
-  return newAr
+  arr.reverse
 end
 
 def kesha_maker(arr)
-  newAr=[]
-  arr.each do |word|
-    word[2]="$"
-    newAr.push(arr)
-  end
+  arr.each{ |word|word[2]="$" }
 end
 
 def find_a(arr)
-  arr.select do |num|
-    num.start_with?("a")
+  arr.select do |word|
+    word.start_with?("a")
   end
 end
 
@@ -69,5 +56,4 @@ def add_s(arr)
   arr.each_with_index.collect do |item, index|
      index !=1 ? item=item + "s" : item=item
   end
-  # puts arr
 end
